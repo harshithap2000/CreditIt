6de6fb3015274d534c921f32d0e9036de9aaab61
Harshitha P <56070328+harshithap2000@users.noreply.github.com>
2020-02-09 14:24:37 +0530

    Add files via upload

--- BuildConfig.java
+++ BuildConfig.java
@@ -1,16 +1,16 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package io.flutter.plugins.firebase_core_web;
+package io.flutter.plugins.googlesignin;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String LIBRARY_PACKAGE_NAME = "io.flutter.plugins.firebase_core_web";
+  public static final String LIBRARY_PACKAGE_NAME = "io.flutter.plugins.googlesignin";
   /**
    * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
    */
   @Deprecated
-  public static final String APPLICATION_ID = "io.flutter.plugins.firebase_core_web";
+  public static final String APPLICATION_ID = "io.flutter.plugins.googlesignin";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = -1;
